f30f1559377d5dcb59f5364bb5e4c8f475a10877
Amulya Kokkireni <amulyakokkireni@gmail.com>
2026-02-24 11:27:09 +0530

    Update skill_extractor.py

--- skill_extractor.py
+++ skill_extractor.py
@@ -1,13 +1,22 @@
-import spacy
+import re
 
-nlp = spacy.load("en_core_web_sm")
+SKILLS_DB = [
+    "python", "java", "c", "c++", "sql", "html", "css", "javascript",
+    "machine learning", "deep learning", "data science",
+    "django", "flask", "streamlit",
+    "git", "github",
+    "linux", "windows",
+    "cloud", "aws", "azure", "gcp",
+    "networking", "cybersecurity",
+    "react", "node", "mysql"
+]
 
-def extract_skills(text, skills_list):
-    doc = nlp(text)
-    extracted_skills = set()
+def extract_skills(text):
+    text = text.lower()
+    skills_found = []
 
-    for token in doc:
-        if token.text in skills_list:
-            extracted_skills.add(token.text)
+    for skill in SKILLS_DB:
+        if re.search(r"\b" + re.escape(skill) + r"\b", text):
+            skills_found.append(skill)
 
-    return extracted_skills
+    return list(set(skills_found))
